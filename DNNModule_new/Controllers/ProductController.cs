0507b278af4d808485c19a7c1c1892b34430e007
Marina Tóth <marina.toth@stud.uni-corvinus.hu>
2025-05-07 19:58:55 +0200

    Add project files.

---
+++ DNNModule_new/Controllers/ProductController.cs
@@ -0,0 +1,102 @@
+﻿using Compare.Dnn.DNNModule_new.Models;
+using CsvHelper;
+using DotNetNuke.Web.Mvc.Framework.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+
+namespace Compare.Dnn.DNNModule_new.Controllers
+{
+    public class ProductController : DnnController
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+           
+                return View();
+            
+
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        
+       
+
+    }
+}
---
+++ DNNModule_new/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Compare.Dnn.DNNModule_new.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string TermekNeve { get; set; }
+        public string Sku { get; set; }
+        public string Ar { get; set; }
+        public string MaximalisVideofelbontas { get; set; }
+        public string MaximalisKepkockasebesseg { get; set; }
+        public string TervezettLefedettseg { get; set; }
+        public string LatoszogFokban { get; set; }
+        public string GpsPozicioNaplozas { get; set; }
+        public string Hangfelvetel { get; set; }
+        public string Nyelvek { get; set; }
+        public string NappaliEszakaiMod { get; set; }
+        public string VideoreszletKivalasztasa { get; set; }
+        public string Tanusitvany { get; set; }
+        public string MemoriakartiyaTipusa { get; set; }
+        public string ObjektivekSzama { get; set; }
+        public string MukodesHomerseklet { get; set; }
+        public string Garancia { get; set; }
+    }
+}
